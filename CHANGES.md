54fb579cfa28e976fd942e7a79dc02dacf40170f
Takahiko Kawasaki <taka@authlete.com>
2023-05-22 03:03:34 +0900

    Added JavaDoc for version 1.0.

--- CHANGES.md
+++
@@ -1,7 +0,0 @@
-CHANGES
-=======
-
-1.0 (2023-05-22)
-----------------
-
-The initial implementation that supports CBOR.
---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.authlete.cbor"},{"l":"com.authlete.cbor.tag"},{"l":"com.authlete.cbor.token"}]
